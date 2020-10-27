db27823671059efbbf0e1ec3e151af3b56030a77
Anny <annylianmarian@gmail.com>
2020-10-27 18:07:53 +0100

    tarea punto 6

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -2,11 +2,14 @@ import React from "react";
 import SearchTable1 from "./SearchTable1";
 import SearchTable2 from "./SearchTable2";
 
-const SearchResults = () => {
+const SearchResults = props => {
   return (
     <table class="table">
       <SearchTable1 />
-      <SearchTable2 />
+      {props.results.map((cliente, index) => {
+        console.log(cliente);
+        return <SearchTable2 cliente={cliente} />;
+      })}
     </table>
   );
 };
--- src/SearchTable2.js
+++ src/SearchTable2.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-const SearchTable2 = () => {
+const SearchTable2 = props => {
+  console.log("cliente", props.cliente);
   return (
     <tbody>
       <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        <td>Mark</td>
+        <th scope="row">{props.cliente.id}</th>
+        <td>{props.cliente.title}</td>
+        <td>{props.cliente.firstName}</td>
+        <td>{props.cliente.surname}</td>
+        <td>{props.cliente.email}</td>
+        <td>{props.cliente.roomId}</td>
+        <td>{props.cliente.checkInDate}</td>
+        <td>{props.cliente.checkOutDate}</td>
       </tr>
     </tbody>
   );
